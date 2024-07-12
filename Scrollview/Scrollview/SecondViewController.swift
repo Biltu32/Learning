02acddcc7efef1dc48bd341505639dc23bdb56c7
Biltu Sarkar <167170041+Biltu32@users.noreply.github.com>
2024-07-13 04:58:28 +0530

    Scrlvw

---
+++ Scrollview/Scrollview/SecondViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SecondViewController.swift
+//  Scrollview
+//
+//  Created by DAPL-Asset-210 on 07/03/24.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
