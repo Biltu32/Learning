cb9be0de138cf1007332d2deb84488cda08f15d8
Biltu Sarkar <167170041+Biltu32@users.noreply.github.com>
2024-07-13 04:53:49 +0530

    Language

---
+++ LocalisationPartnerApp/LocalisationPartnerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LocalisationPartnerApp
+//
+//  Created by DAPL-Asset-210 on 08/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
